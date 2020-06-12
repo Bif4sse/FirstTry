029402c39f593020cc7f5802ff6670bf402d2fba
Anthony Fabis <fabis.anthony@gmail.com>
2020-06-12 18:11:34 +0200

    //penis

---
+++ src/main/java/ultima/firsttry/FirstTry.java
@@ -0,0 +1,18 @@
+package ultima.firsttry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FirstTry extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //bite
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
